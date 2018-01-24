b23e7aa1ca3e619ea128e9ba4fc04de24a735c6f
Gyula Halmos <gyula.halmos@gmail.com>
2018-01-24 16:03:45 -0500

    Add new functions to vector
    Add addition, subtraction, multiplication, magnitude and normali
    zation.

--- vector.py
+++ vector.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -12,10 +15,30 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, v):
+        return Vector([x+y for x, y in zip(self.coordinates, v.coordinates)])
+
+    def __sub__(self, v):
+        return Vector([numpy.subtract(self.coordinates, v.coordinates)])
+
+    def __mul__(self, scalar):
+        return Vector([scalar * x for x in self.coordinates])
+
+    def __rmul__(self, scalar):
+        return self * scalar
+
+    def magnitude(self):
+        return numpy.round(numpy.sqrt(numpy.sum([x*x for x in self.coordinates])),3)
+
+    def normalize(self):
+        try:
+            magnitude = self.magnitude()
+            return 1./magnitude * self
+        except ZeroDivisionError:
+            print("Cannot divide ZERO vector!!!")
